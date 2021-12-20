01f38107512dfeb2ae2099ce5b6d3aaa33aa4c37
leochastagnac <leochastagnac@gmail.com>
2021-12-20 11:58:52 +0100

    init backend

---
+++ product/serializers.py
@@ -0,0 +1,33 @@
+
+from rest_framework import serializers
+
+from .models import Category, Product
+
+class ProductSerializer(serializers.ModelSerializer):
+    class Meta: 
+        model = Product 
+        fields = (
+            "id",
+            "category",
+            "name", 
+            "get_absolute_url",
+            "description",
+            "price",
+            "get_image",
+            "get_thumbnail",
+            "energy_values",
+            "fatty_substances",
+            "carbs",
+            "proteins",
+        ) 
+
+class CategorySerializer(serializers.ModelSerializer):
+    products = ProductSerializer(many=True)
+    class Meta:
+        model = Category
+        fields = (
+            "id",
+            "name",
+            "get_absolute_url",
+            "products"
+        )
---
+++ product/urls.py
@@ -0,0 +1,13 @@
+#Products URL Configuration
+
+from django.urls import path
+from product import views
+
+
+urlpatterns = [
+    path('latest-products/', views.LatestProductsList.as_view()),
+    path('products/search/', views.search),
+    path('needed-products/<slug:usability_slug>/', views.NeededProductList.as_view()),
+    path('products/<slug:category_slug>/<slug:product_slug>/', views.ProductDetail.as_view()),
+    path('products/<slug:category_slug>/', views.CategoryDetail.as_view()),
+]
